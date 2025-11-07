3eb9fad4ee1107441d61cbf7cfc5ac6247ddb469
Ieysn_s <139177436+Efsann@users.noreply.github.com>
2025-11-07 23:03:34 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,148 +1,158 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const inputDay = document.querySelector('.input-days');
-  const inputMonth = document.querySelector('.input-months');
-  const inputYear = document.querySelector('.input-years');
+  const gunGiris = document.querySelector('.input-days');
+  const ayGiris = document.querySelector('.input-months');
+  const ilGiris = document.querySelector('.input-years');
   const button = document.querySelector('.calculator__user-input-btn');
-  inputDay.addEventListener('input', function () {
-    if (inputDay.nextElementSibling.innerHTML == "Must be a valid date") {
-      inputDay.parentElement.classList.remove("error");
-      inputMonth.parentElement.classList.remove("error");
-      inputYear.parentElement.classList.remove("error");
-    }
-    if (inputDay.value > 31) {
-      inputDay.nextElementSibling.innerHTML = "Must be a valid day"
-      inputDay.parentElement.classList.add("error");
-    } else {
-      inputDay.parentElement.classList.remove("error");
-    }
-  });
-  inputMonth.addEventListener('input', function () {
-    if (inputDay.nextElementSibling.innerHTML == "Must be a valid date") {
-      inputDay.parentElement.classList.remove("error");
-      inputMonth.parentElement.classList.remove("error");
-      inputYear.parentElement.classList.remove("error");
-    }
-    if (inputMonth.value > 12) {
-      inputMonth.nextElementSibling.innerHTML = "Must be a valid mounth"
-      inputMonth.parentElement.classList.add("error");
-    } else {
-      inputMonth.parentElement.classList.remove("error");
 
+  gunGiris.addEventListener('input', function () {
+    if (gunGiris.nextElementSibling.innerHTML == "Must be a valid date") {
+      gunGiris.parentElement.classList.remove("error");
+      ayGiris.parentElement.classList.remove("error");
+      ilGiris.parentElement.classList.remove("error");
     }
-  });
-  inputYear.addEventListener('input', function () {
-    if (inputDay.nextElementSibling.innerHTML == "Must be a valid date") {
-      inputDay.parentElement.classList.remove("error");
-      inputMonth.parentElement.classList.remove("error");
-      inputYear.parentElement.classList.remove("error");
-    }
-    let Data = new Date();
-    if (inputYear.value > Data.getFullYear()) {
-      inputYear.nextElementSibling.innerHTML = "Must be in the past"
-      inputYear.parentElement.classList.add("error");
+    if (gunGiris.value > 31) {
+      gunGiris.nextElementSibling.innerHTML = "Must be a valid day";
+      gunGiris.parentElement.classList.add("error");
     } else {
-      inputYear.parentElement.classList.remove("error");
+      gunGiris.parentElement.classList.remove("error");
     }
   });
-  inputYear.addEventListener('change', function () {
-    let Data = new Date();
-    if (inputYear.value < 0) {
-      inputYear.value = -inputYear.value;
-      inputYear.value = Data.getFullYear() - inputYear.value;
+  ayGiris.addEventListener('input', function () {
+    if (gunGiris.nextElementSibling.innerHTML == "Must be a valid date") {
+      gunGiris.parentElement.classList.remove("error");
+      ayGiris.parentElement.classList.remove("error");
+      ilGiris.parentElement.classList.remove("error");
+    }
+    if (ayGiris.value > 12) {
+      ayGiris.nextElementSibling.innerHTML = "Must be a valid mounth";
+      ayGiris.parentElement.classList.add("error");
+    } else {
+      ayGiris.parentElement.classList.remove("error");
+    }
+  });
+  ilGiris.addEventListener('input', function () {
+    if (gunGiris.nextElementSibling.innerHTML == "Must be a valid date") {
+      gunGiris.parentElement.classList.remove("error");
+      ayGiris.parentElement.classList.remove("error");
+      ilGiris.parentElement.classList.remove("error");
+    }
+    const bugunTarix = new Date();
+    if (ilGiris.value > bugunTarix.getFullYear()) {
+      ilGiris.nextElementSibling.innerHTML = "Must be in the past";
+      ilGiris.parentElement.classList.add("error");
+    } else {
+      ilGiris.parentElement.classList.remove("error");
+    }
+  });
+
+ 
+
+  
+  ilGiris.addEventListener('change', function () {
+    const bugunTarix = new Date();
+    if (ilGiris.value < 0) {
+      ilGiris.value = -ilGiris.value;
+      ilGiris.value = bugunTarix.getFullYear() - ilGiris.value;
     }
   });
   button.addEventListener('click', function () {
-    day = inputDay.value;
-    month = inputMonth.value;
-    year = inputYear.value;
-    if (!day) {
-      inputDay.parentElement.classList.add("error");
-      inputDay.nextElementSibling.innerHTML = "This field is required"
+    const gun = Number(gunGiris.value);
+    const ay = Number(ayGiris.value);
+    const il = Number(ilGiris.value);
+    if (!gun) {
+      gunGiris.parentElement.classList.add("error");
+      gunGiris.nextElementSibling.innerHTML = "This field is required";
     }
-    if (!month) {
-      inputMonth.parentElement.classList.add("error");
-      inputMonth.nextElementSibling.innerHTML = "This field is required"
+    if (!ay) {
+      ayGiris.parentElement.classList.add("error");
+      ayGiris.nextElementSibling.innerHTML = "This field is required";
     }
-    if (!year) {
-      inputYear.parentElement.classList.add("error");
-      inputYear.nextElementSibling.innerHTML = "This field is required"
+    if (!il) {
+      ilGiris.parentElement.classList.add("error");
+      ilGiris.nextElementSibling.innerHTML = "This field is required";
     }
-    if (!day || !month || !year) {
+    if (!gun || !ay || !il) {
       return;
     }
-    let date = new Date(year, month - 1, day);
-    let currentData = new Date();
-//Checking if the date is correct
-    if (!(date.getFullYear() == year && date.getMonth() == month - 1 && date.getDate() == day) || (document.getElementsByClassName('error').length) || date > currentData || year < 0) {
-      inputDay.parentElement.classList.add("error");
-      inputDay.nextElementSibling.innerHTML = "Must be a valid date"
-      inputMonth.parentElement.classList.add("error");
-      inputMonth.nextElementSibling.innerHTML = ""
-      inputYear.parentElement.classList.add("error");
-      inputYear.nextElementSibling.innerHTML = "";
+    const dogumTarix = new Date(il, ay - 1, gun);
+    const indikiTarix = new Date();
+    if (
+      !(dogumTarix.getFullYear() == il &&
+        dogumTarix.getMonth() == ay - 1 &&
+        dogumTarix.getDate() == gun) ||
+      document.getElementsByClassName('error').length ||
+      dogumTarix > indikiTarix ||
+      il < 0
+    ) {
+      gunGiris.parentElement.classList.add("error");
+      gunGiris.nextElementSibling.innerHTML = "Must be a valid date";
+      ayGiris.parentElement.classList.add("error");
+      ayGiris.nextElementSibling.innerHTML = "";
+      ilGiris.parentElement.classList.add("error");
+      ilGiris.nextElementSibling.innerHTML = "";
       return;
     }
+    let yasIl = indikiTarix.getFullYear() - dogumTarix.getFullYear();
+    let yasAy = 0;
+    let yasGun = 0;
 
-
-
-    let age_year = currentData.getFullYear() - date.getFullYear();
-    let age_mounth = 0;
-    let age_day = 0;
-    if (currentData < new Date(currentData.getFullYear(), month - 1, day)) {
-      age_year = age_year - 1;
-      age_mounth = currentData.getMonth() + 1;
-      age_day = currentData.getDate();
+    if (indikiTarix < new Date(indikiTarix.getFullYear(), ay - 1, gun)) {
+      yasIl = yasIl - 1;
+      yasAy = indikiTarix.getMonth() + 1;
+      yasGun = indikiTarix.getDate();
     } else {
-      if (currentData.getMonth() + 1 === month) {
-        age_mounth = 0;
-        age_day = currentData.getDate() - day;
-        console.log(age_day);
+      if (indikiTarix.getMonth() + 1 === ay) {
+        yasAy = 0;
+        yasGun = indikiTarix.getDate() - gun;
       } else {
-        age_mounth = currentData.getMonth() + 1 - month;
-        if (currentData.getDate() < day) {
-          age_mounth = age_mounth - 1;
-          age_day = currentData.getDate() + new Date(currentData.getFullYear(), currentData.getMonth(), 0).getDate() - day;
+        yasAy = indikiTarix.getMonth() + 1 - ay;
+        if (indikiTarix.getDate() < gun) {
+          yasAy = yasAy - 1;
+          yasGun =
+            indikiTarix.getDate() +
+            new Date(
+              indikiTarix.getFullYear(),
+              indikiTarix.getMonth(),
+              0
+            ).getDate() -
+            gun;
         } else {
-          age_day = currentData.getDate() - day;
+          yasGun = indikiTarix.getDate() - gun;
         }
       }
-
     }
-
-    
-    const outputDay = document.querySelector('.output-days').querySelector('span');
-    const outputMonth = document.querySelector('.output-months').querySelector('span');
-    const outputYear = document.querySelector('.output-years').querySelector('span');
-
-    OutputNumber(outputYear, age_year);
-    OutputNumber(outputMonth, age_mounth);
-    OutputNumber(outputDay, age_day);
-
+    const cixisGun = document
+      .querySelector('.output-days')
+      .querySelector('span');
+    const cixisAy = document
+      .querySelector('.output-months')
+      .querySelector('span');
+    const cixisIl = document
+      .querySelector('.output-years')
+      .querySelector('span');
+    reqemiAnimasiyaEt(cixisIl, yasIl);
+    reqemiAnimasiyaEt(cixisAy, yasAy);
+    reqemiAnimasiyaEt(cixisGun, yasGun);
   });
 
-  
-  function OutputNumber(el, num) {
+  function reqemiAnimasiyaEt(element, reqem) {
     let step = 50;
-    num > 25 && (step = 35);
-    num > 50 && (step = 25);
-    num > 75 && (step = 20);
-    num > 100 && (step = 10);
-    num > 200 && (step = 1);
-
-    let n = 0;
-    if (num === 0) {
-      el.innerHTML = n;
+    reqem > 25 && (step = 35);
+    reqem > 50 && (step = 25);
+    reqem > 75 && (step = 20);
+    reqem > 100 && (step = 10);
+    reqem > 200 && (step = 1);
+    let cariReqem = 0;
+    if (reqem === 0) {
+      element.innerHTML = cariReqem;
     } else {
-      let inteval = setInterval(() => {
-        n = n + 1;
-        if (n === num) {
-          clearInterval(inteval);
+      const intervalId = setInterval(() => {
+        cariReqem = cariReqem + 1;
+        if (cariReqem === reqem) {
+          clearInterval(intervalId);
         }
-        el.innerHTML = n;
+        element.innerHTML = cariReqem;
       }, step);
     }
-
   }
-
 });
-
